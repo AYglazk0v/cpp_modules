dab9f7315664138f57a28517f7a8448636c92f63
gtaggana <gtaggana@student.school21.ru>
2022-09-05 19:30:44 +0300

    using itterator and inline

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
+namespace {
+	inline void printWordToUpper(const std::string& str) {
+		for (std::string::const_iterator it = str.begin(), ite = str.end(); it != ite; ++it) {
+			std::cout<< static_cast<char>(toupper(*it));
+		}
+	}
+}
+
 int	main(int argc, char **argv) {
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	} else {
 		std::string out;
-		for (int i = 1; i < argc; i++) {
-			for (int j = 0; argv[i][j]; j++) {
-				std::cout<< (char) toupper(argv[i][j]);
-			}
+		for (int i = 1; i < argc; ++i) {
+			printWordToUpper(static_cast<std::string>(argv[i]));
 		}
 		std::cout<<std::endl;
 	}
